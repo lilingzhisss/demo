3b3c3eb8a7c023795206f444d252230ad66a9f1e
llz <lilingzhisss@163.com>
2017-09-22 18:33:12 +0800

    sasasasa

--- demo/AppDelegate.m
+++ demo/AppDelegate.m
@@ -18,6 +18,7 @@
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
 //    ssss
+//    kSecKeyAlgorithmECDSASignatureDigestX962SHA1
     return YES;
 }
 
