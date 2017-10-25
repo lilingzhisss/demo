b1becb51c660c3efdc9aef4605793596c3ea4846
llz <lilingzhisss@163.com>
2017-10-25 20:03:00 +0800

    Revert "333333"
    This reverts commit 66ecf1db3be3691c29cc2eba0213062e5c702c68.

--- demo/AppDelegate.m
+++ demo/AppDelegate.m
@@ -16,7 +16,6 @@
 
 //1
 //2
-//3
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
 
